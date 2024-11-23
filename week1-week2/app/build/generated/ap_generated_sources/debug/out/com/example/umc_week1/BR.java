f788e8094c53c1f009cb4c04908102d22d007403
OSSU <90602694+SuHyeon00@users.noreply.github.com>
2024-11-23 19:52:43 +0900

    Delete week1-week2/app/build directory

--- week1-week2/app/build/generated/ap_generated_sources/debug/out/com/example/umc_week1/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.umc_week1;
-
-public class BR {
-  public static final int _all = 0;
-}
--- week1-week2/app/build/generated/data_binding_trigger/debug/com/example/umc_week1/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.umc_week1;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
